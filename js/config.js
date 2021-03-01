9bf2da9dc92a222ea368c266f6db0bd4190b5d7a
lfontalvo <lfontalvo@mueblesjamar.com.co>
2021-03-01 08:51:20 -0500

    configuracion cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_qpGFpubUZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2jv6ope9rep8jdlf9mf0e4ojco', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
